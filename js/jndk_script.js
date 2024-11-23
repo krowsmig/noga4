3701915986e006ba4cb3a25cb970f09efde54aa5
krowsmig <186128055+krowsmig@users.noreply.github.com>
2024-11-24 06:37:35 +0900

    Update jndk_script.js

--- js/jndk_script.js
+++ js/jndk_script.js
@@ -2,13 +2,15 @@ $(document).ready(function(){
 
 
 				var pd_btm  = $("#img_title_1 .img_title_div > .pd_tp_bm_300").outerHeight() - $("#img_title_1 .img_title_div > .pd_tp_bm_300").height();
-
 				var top_height = $("#gnb").outerHeight() + $("#main").outerHeight() + $("#target").outerHeight() - ( pd_btm / 2 );
-				var now = $(window).scrollTop();
-				console.log(top_height);
-
 				var top_height_2 = top_height + $("#img_title_1").outerHeight();
 				
+				$(window).resize(function(){
+					pd_btm  = $("#img_title_1 .img_title_div > .pd_tp_bm_300").outerHeight() - $("#img_title_1 .img_title_div > .pd_tp_bm_300").height();
+					top_height = $("#gnb").outerHeight() + $("#main").outerHeight() + $("#target").outerHeight() - ( pd_btm / 2 );
+					top_height_2 = top_height + $("#img_title_1").outerHeight();
+				});
+
 				
 				$(window).scroll(function(){
 					if ($(this).scrollTop() > top_height) {
@@ -32,27 +34,5 @@ $(document).ready(function(){
 				
 				
 				});	
-				// #content_3 list click event
-				$("#content_3 ul li a").click(function(){
-					$(this).parent().addClass("on");
-					$(this).parent().siblings().removeClass("on");
-
-					return false;
-				});
-
-				// pagination
-				$(".pagination a").click(function(){
-					$(this).siblings().removeClass("on");
-					$(this).addClass("on");
-
-					var num = $(this).index();
-					
-					$("#step_ul").removeClass();
-					$("#step_ul").addClass("num_"+num);
-
-					return false;
-				});
-				
-
 					
 			});
