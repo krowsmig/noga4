ff8aa17783c0d5ae5dcc1723745cdef281088dc0
krowsmig <186128055+krowsmig@users.noreply.github.com>
2024-11-24 11:11:03 +0900

    Update sub_script.js

--- js/sub_script.js
+++ js/sub_script.js
@@ -8,29 +8,42 @@ $(document).ready(function(){
 	});
 
 	
-
-	var getHeights = $("#main").outerHeight() -300;
+	var getPdBtm = $(".quick_div.pd_btm_160").outerHeight() - $(".quick_div.pd_btm_160").height();
+	var getPdBtm_2 = getPdBtm * 2;
+	var getHeights = $("#main").outerHeight() - getPdBtm_2;
+	
 	$(window).resize(function(){ 
-		getHeights = $("#main").outerHeight() -300;
+		getPdBtm = $(".quick_div.pd_btm_160").outerHeight() - $(".quick_div.pd_btm_160").height();
+		getPdBtm_2 = getPdBtm * 2;
+		getHeights = $("#main").outerHeight() - getPdBtm_2;
 	});
+
 	$(function(){
 		$(window).scroll(function(){
 			if ($(this).scrollTop() > getHeights) {
-
+				
 				$(".target_ul").addClass("chart_ani");
 
-				setTimeout(function(){
-					$(".target_ul").addClass("after");
-				}, 4000);
-			} else {
-				// $(".target_ul").removeClass("chart_ani");
-				// $(".target_ul").removeClass("after");
-			}
-		}).resize(function(){ 
-			if ($(this).width() < 1024) {
+				if ($(".target_ul").hasClass("after")) {
+
+				} else {
+
+					setTimeout(function(){
+
+						$(".target_ul").addClass("after");
+						
+					}, 4000);
+				}
+			} else if ($(this).scrollTop() < getHeights) {
+				
+				$(".target_ul").removeClass("after");
+				$(".target_ul").removeClass("chart_ani");
+
 				
 			} else {
-
+				
+				$(".target_ul").removeClass("chart_ani");
+				$(".target_ul").removeClass("after");
 			}
 		});
 	});
