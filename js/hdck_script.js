d7c62b9e3c37ee20b5a29ce563b416f1e68fc25a
krowsmig <186128055+krowsmig@users.noreply.github.com>
2024-11-24 11:08:43 +0900

    Update hdck_script.js

--- js/hdck_script.js
+++ js/hdck_script.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
-  var pd_btm  = $(".img_title_div").outerHeight() - $(".img_title_div").height();
+    var pd_btm  = $(".img_title_div").outerHeight() - $(".img_title_div").height();
+    var de_li  = $(".design_li").outerHeight();
 
-  var top_height = $("#gnb").outerHeight() + $("#main").outerHeight() + $("#target").outerHeight() - (pd_btm  * 4 );
-  var top_height_2 = top_height + $("#img_title_1").outerHeight() + $("#content_1").outerHeight() + (pd_btm  * 2 );
-  var top_height_3 = top_height_2 + $("#img_title_2").outerHeight() + $("#content_2").outerHeight() + (pd_btm  * 3 );
-  var top_height_4 = top_height_3 + $("#img_title_3").outerHeight() + $("#content_3").outerHeight() + (pd_btm  * 2 );
-  
+    var top_height = $("#gnb").outerHeight() + $("#main").outerHeight() + $("#target").outerHeight() - de_li;
+    var top_height_2 = top_height + $("#img_title_1").outerHeight() + $("#content_1").outerHeight() + de_li - (pd_btm  * 2 );
+    var top_height_3 = top_height_2 + $("#img_title_2").outerHeight() + $("#content_2").outerHeight() + (pd_btm  * 3 );
+    var top_height_4 = top_height_3 + $("#img_title_3").outerHeight() + $("#content_3").outerHeight() + (pd_btm  * 2 );
+    
 
-  $(window).resize(function(){ 
-    pd_btm  = $(".img_title_div").outerHeight() - $(".img_title_div").height();
-    top_height = $("#gnb").outerHeight() + $("#main").outerHeight() + $("#target").outerHeight() - (pd_btm * 4 );
-    top_height_2 = top_height + $("#img_title_1").outerHeight() + $("#content_1").outerHeight() + (pd_btm  * 2 );
-    top_height_3 = top_height_2 + $("#img_title_2").outerHeight() + $("#content_2").outerHeight() + (pd_btm  * 3 );
-    top_height_4 = top_height_3 + $("#img_title_3").outerHeight() + $("#content_3").outerHeight() + (pd_btm  * 2 );
-    console.log(pd_btm);
+    $(window).resize(function(){ 
+      pd_btm  = $(".img_title_div").outerHeight() - $(".img_title_div").height();
+      de_li  = $(".design_li").outerHeight();
 
-  });
-  
+      top_height = $("#gnb").outerHeight() + $("#main").outerHeight() + $("#target").outerHeight() - de_li;
+      top_height_2 = top_height + $("#img_title_1").outerHeight() + $("#content_1").outerHeight() + de_li - (pd_btm  * 2 );
+      top_height_3 = top_height_2 + $("#img_title_2").outerHeight() + $("#content_2").outerHeight() + (pd_btm  * 3 );
+      top_height_4 = top_height_3 + $("#img_title_3").outerHeight() + $("#content_3").outerHeight() + (pd_btm  * 2 );
+     
+
+    });
+    
   $(".img_title_div").addClass("show_now");
 
   $(window).scroll(function(){
@@ -25,7 +28,7 @@ $(document).ready(function(){
       $("#img_title_1 .img_title_div").removeClass("show_now");
 
     }  else {
-
+      $("#img_title_1 .img_title_div").addClass("show_now");
     }
 
 
@@ -34,6 +37,7 @@ $(document).ready(function(){
       $("#img_title_2 .img_title_div").removeClass("show_now");
 
     } else {
+      $("#img_title_2 .img_title_div").addClass("show_now");
 
     }
 
@@ -42,14 +46,16 @@ $(document).ready(function(){
       $("#img_title_3 .img_title_div").removeClass("show_now");
 
     } else {
+      $("#img_title_3 .img_title_div").addClass("show_now");
 
     }
 
     if ($(this).scrollTop() > top_height_4) {
-      // alert(top_height_2);
+      
       $("#img_title_4 .img_title_div").removeClass("show_now");
 
     } else {
+      $("#img_title_4 .img_title_div").addClass("show_now");
 
     }
 
